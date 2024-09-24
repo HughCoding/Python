1278d51764cb2c4fff8a15bdeff9d54270e4b204
HughCoding <160680383+HughCoding@users.noreply.github.com>
2024-09-24 20:56:15 -0300

    Add files via upload

---
+++ automacao/app.py
@@ -0,0 +1,57 @@
+import openpyxl
+from selenium import webdriver
+from selenium.webdriver.common.by import By  
+from time import sleep
+
+
+planilha_clientes = openpyxl.load_workbook('C:/Users/55279/Desktop/Python/automacao/dados_clientes.xlsx')
+pagina_clientes = planilha_clientes['Sheet1']
+
+
+driver = webdriver.Chrome()
+driver.get('https://consultcpf-devaprender.netlify.app')
+
+
+planilha_fechamento = openpyxl.load_workbook('C:/Users/55279/Desktop/Python/automacao/planilha_fechamento.xlsx')
+pagina_fechamento = planilha_fechamento['Sheet1']
+
+
+for linha in pagina_clientes.iter_rows(min_row=2, values_only=True):
+    nome, valor, cpf, vencimento = linha
+    sleep(5)  
+
+
+    campo_pesquisa = driver.find_element(By.XPATH, "//input[@id='cpfInput']")
+    sleep(1)
+    campo_pesquisa.clear()
+    campo_pesquisa.send_keys(cpf)
+    sleep(1)
+    botao_pesquisar = driver.find_element(By.XPATH, "//button[@class='btn btn-custom btn-lg btn-block mt-3']")
+    sleep(1)
+    botao_pesquisar.click()
+    sleep(4)
+
+
+    status = driver.find_element(By.XPATH, "//span[@id='statusLabel']")
+    if status.text == 'em dia':
+
+        data_pagamento = driver.find_element(By.XPATH, "//p[@id='paymentDate']")
+        metodo_pagamento = driver.find_element(By.XPATH, "//p[@id='paymentMethod']")
+        
+        data_pagamento_limpo = data_pagamento.text.split()[3]
+        metodo_pagamento_limpo = metodo_pagamento.text.split()[3]
+    
+        pagina_fechamento.append([nome, valor, cpf, vencimento, 'em dia', data_pagamento_limpo,
+                                  metodo_pagamento_limpo])
+        
+        planilha_fechamento.save('planilha_fechamento.xlsx')
+    else:
+        planilha_fechamento = openpyxl.load_workbook('planilha_fechamento.xlxs')
+        planilha_fechamento = planilha_fechamento['Sheet1']
+        
+        
+        pagina_fechamento.append([nome, valor, cpf, vencimento, 'pendente'])
+        planilha_fechamento.save('planilha_fechamento.xlsx')
+
+
+driver.quit()
